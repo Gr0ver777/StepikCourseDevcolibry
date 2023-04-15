7add26b4e4eb741a088719fa3d1f30aa3b23d9ae
grover7777@yandex.ru <grafmontekrusto>
2023-04-15 14:23:06 +0300

    lesson3 add data layout

---
+++ app/src/main/java/com/example/stepikcoursedevcolibry/data/ShopListRepositoryImp.kt
@@ -0,0 +1,39 @@
+package com.example.stepikcoursedevcolibry.data
+
+import com.example.stepikcoursedevcolibry.domein.ShopItem
+import com.example.stepikcoursedevcolibry.domein.ShopListRepository
+
+object ShopListRepositoryImp: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID){
+            shopItem.id = autoIncrementId++}
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getObjectIdItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getObjectIdItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId } ?:
+            throw RuntimeException(
+                "Element this id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
